e8c58a50df50368cfb749a1a52f6b32370ccc937
Praneethysp007 <praneeth676yerram@gmail.com>
2024-01-13 18:46:38 +0530

    variables1

--- variables.sh
+++ variables.sh
@@ -5,3 +5,7 @@ name=ysp
 echo "this is the project of ${name},done by the expertise of ${name}"
 
 echo "its can also be used without curly braces is it right $name"
+
+number=5
+
+echo "$number + 1"
